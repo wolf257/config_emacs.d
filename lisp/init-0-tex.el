e6f988316826125c48a61811b60735640f33f0e0
Usain-mac <wolf257@users.noreply.github.com>
2018-03-20 10:03:10 +0100

    add myarticle for lua article in org-latex-classes

--- lisp/init-0-tex.el
+++ lisp/init-0-tex.el
@@ -37,6 +37,8 @@
 ;;              '("lualatex -shell-escape -interaction nonstopmode %f")
 ;; )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq org-latex-pdf-process
       '("lualatex -shell-escape -interaction nonstopmode %f"
         "lualatex -shell-escape -interaction nonstopmode %f"))
@@ -48,5 +50,90 @@
 ;;         "pdflatex -interaction nonstopmode -output-directory %o %f" )
 ;;       )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;; class par default. default = article
+(setq org-latex-default-class "article")
+
+(setq org-latex-classes
+      '( ;;; myClasses
+        ("myarticle" "\\documentclass[11pt, a4paper]{article}
+                % input
+                \\usepackage[utf8]{luainputenc}
+                \\usepackage{fontspec}
+                \\usepackage{unicode-math}
+                \\usepackage[novoc]{arabluatex}
+                \\newfontfamily\\arabicfont[Script=Arabic]{Scheherazade}
+                \\newcommand{\\arbt}[1]{\\Large{\\arb{#1}}} %{\\arbt{...}}
+                \\newcommand{\\arbT}[1]{\\LARGE\\arb{#1}} %\\arbT{...}
+                % layout
+                \\usepackage{layout}
+                \\usepackage{setspace} %package pour les interlignes
+                \\usepackage{multicol}
+                \\setlength{\\columnsep}{40pt}
+                \\usepackage{xcolor} % package pour les couleurs
+                % headers ...
+                \\usepackage{fancyhdr} % header etc
+                \\usepackage{fancybox} %package pour les encadrements supp
+                \\pagestyle{fancy} % Use en-tetes et des pieds de page personnaliser grâce fancyhdr
+                \\usepackage[Glenn]{fncychap}
+                \\fancyhead[L, R, C]{} % définition en-tête
+                \\fancyfoot[L, R, C]{} % définition pied de page
+         		\\fancyhf{} % on efface tout
+        		\\renewcommand{\\headrulewidth}{0pt} % ligne haut out
+	        	\\renewcommand{\\footrulewidth}{0pt} % ligne bas out
+                % others
+                \\usepackage{float}
+                \\usepackage{array, makecell} %pour les tableaux et makecell pour les cellules
+                \\usepackage{multirow} %pr tableau
+                \\newcolumntype{P}[1]{>{\\centering\\arraybackslash}m{#1}} %P{taille}
+                % maths
+                \\usepackage{amsmath, amsthm} %package pour les maths
+                \\usepackage{lettrine}
+                \\usepackage{marvosym} %package de symbole
+                % others
+                \\usepackage{wrapfig} % pour les capsules in text
+                %defaut emacs packages
+                \\usepackage{fixltx2e}
+                \\usepackage{graphicx}
+                \\usepackage{longtable}
+                \\usepackage{float}
+                \\usepackage{wrapfig}
+                \\usepackage{rotating}
+                \\usepackage[normalem]{ulem}
+                \\usepackage{textcomp}
+                \\usepackage{wasysym}
+                \\usepackage{hyperref}
+               [NO-DEFAULT-PACKAGES]
+               [PACKAGES]"
+
+         ("\\section{%s}" . "\\section*{%s}")
+         ("\\subsection{%s}" . "\\subsection*{%s}")
+         ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+         ("\\paragraph{%s}" . "\\paragraph*{%s}")
+         ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+         ;;; default classes
+        ("article" "\\documentclass[11pt]{article}"
+         ("\\section{%s}" . "\\section*{%s}")
+         ("\\subsection{%s}" . "\\subsection*{%s}")
+         ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+         ("\\paragraph{%s}" . "\\paragraph*{%s}")
+         ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+         ;;;;;;;
+        ("report" "\\documentclass[11pt]{report}"
+         ("\\part{%s}" . "\\part*{%s}")
+         ("\\chapter{%s}" . "\\chapter*{%s}")
+         ("\\section{%s}" . "\\section*{%s}")
+         ("\\subsection{%s}" . "\\subsection*{%s}")
+         ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+         ;;;;;;;
+        ("book" "\\documentclass[11pt]{book}"
+         ("\\part{%s}" . "\\part*{%s}")
+         ("\\chapter{%s}" . "\\chapter*{%s}")
+         ("\\section{%s}" . "\\section*{%s}")
+         ("\\subsection{%s}" . "\\subsection*{%s}")
+         ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+         ;;;;;;;
+        ))
 
 (provide 'init-0-tex)
